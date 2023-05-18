da0c657ba56ad6a31adb66b6bda1e17d8706a8f1
vlad_Chaikovskyi <hight1337@gmail.com>
2023-05-19 02:25:28 +0300

    customize not found page

--- src/routes/NotFound.tsx
+++ src/routes/NotFound.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { FC } from "react";
+// libs
+import { Button, Result } from "antd";
+import { useNavigate } from "react-router-dom";
+// constants
+import { MAIN_ROUTES } from "constants/routes";
 
-function NotFound() {
-  return <div>NotFound</div>;
-}
+const NotFound: FC = () => {
+  const navigate = useNavigate();
+  const handleBackHome = () => {
+    navigate(MAIN_ROUTES.HOME, { replace: true });
+  };
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Button onClick={handleBackHome} type="primary">
+          Back Home
+        </Button>
+      }
+    />
+  );
+};
 
 export default NotFound;
